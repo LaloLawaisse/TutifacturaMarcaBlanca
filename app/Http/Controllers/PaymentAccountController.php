08a6eaeb6a2135fd654e56e8a56922e25e4807a2
LaloLawaisse <lisandrolawaisse@gmail.com>
2026-01-19 02:59:26 -0300

    Create PaymentAccountController.php

---
+++ app/Http/Controllers/PaymentAccountController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaymentAccountController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['message' => 'Not implemented'], 501);
+    }
+
+    public function create()
+    {
+        return response()->json(['message' => 'Not implemented'], 501);
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json(['message' => 'Not implemented'], 501);
+    }
+
+    public function show($id)
+    {
+        return response()->json(['message' => 'Not implemented'], 501);
+    }
+
+    public function edit($id)
+    {
+        return response()->json(['message' => 'Not implemented'], 501);
+    }
+
+    public function update(Request $request, $id)
+    {
+        return response()->json(['message' => 'Not implemented'], 501);
+    }
+
+    public function destroy($id)
+    {
+        return response()->json(['message' => 'Not implemented'], 501);
+    }
+}
